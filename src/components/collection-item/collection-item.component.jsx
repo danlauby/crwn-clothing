09e908120b59df0695d05f25202ef777f1614742
Daniel Lauby <dmlauby@gmail.com>
2019-09-09 14:54:24 -0700

    Create header component, link options to home, shop and contact pages. Fix indent spacing.

--- src/components/collection-item/collection-item.component.jsx
+++ src/components/collection-item/collection-item.component.jsx
@@ -12,10 +12,10 @@ const CollectionItem = ({ id, name, price, imageUrl }) => (
       }}>
     </div>
 
-      <div className="collection-footer">
-        <span className="name">{name}</span>
-        <span className="price">{price}</span>
-      </div>
+    <div className="collection-footer">
+      <span className="name">{name}</span>
+      <span className="price">{price}</span>
+    </div>
   </div>
 )
 
--- src/components/collection-preview/collection-preview.component.jsx
+++ src/components/collection-preview/collection-preview.component.jsx
@@ -6,16 +6,16 @@ import CollectionItem from '../collection-item/collection-item.component';
 
 const CollectionPreview = ({ title, items }) => (
   <div className="collection-preview">
-  <h1 className="title">{title.toUpperCase()}</h1>
-  <div className="preview">
-    {
-      items
-      .filter((item, idx) => idx < 4)
-      .map(({ id, ...otherCollectionItemProps }) => (
-        <CollectionItem key={id} { ...otherCollectionItemProps }/>
-      ))
-    }
-  </div>
+    <h1 className="title">{title.toUpperCase()}</h1>
+    <div className="preview">
+      {
+        items
+        .filter((item, idx) => idx < 4)
+        .map(({ id, ...otherCollectionItemProps }) => (
+          <CollectionItem key={id} { ...otherCollectionItemProps }/>
+        ))
+      }
+    </div>
   </div>
 )
 
