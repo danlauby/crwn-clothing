cf81aa2d58721480b51071ff2e582a421560ca33
Daniel Lauby <dmlauby@gmail.com>
2019-09-19 11:51:22 -0700

    Update shop data and create cart utils to group cart items in cart reducer.

--- src/components/button/button.component.jsx
+++ src/components/button/button.component.jsx
@@ -3,8 +3,8 @@ import React from 'react';
 import './button.styles.scss';
 
 
-const Button = ({ children, isGoogleSignIn, ...otherProps }) => (
-  <button className={`${isGoogleSignIn ? 'google-sign-in' : ''} custom-button`} {...otherProps}>{children}</button>
+const Button = ({ children, isGoogleSignIn, inverted, ...otherProps }) => (
+  <button className={`${inverted ? 'inverted' : ''} ${isGoogleSignIn ? 'google-sign-in' : ''} custom-button`} {...otherProps}>{children}</button>
 )
 
 
--- src/components/collection-item/collection-item.component.jsx
+++ src/components/collection-item/collection-item.component.jsx
@@ -1,23 +1,36 @@
 import React from 'react';
+import { connect } from 'react-redux';
+
+import { addItem } from '../../redux/cart/cart.actions';
+
+import Button from '../button/button.component';
 
 import './collection-item.styles.scss';
 
 
-const CollectionItem = ({ id, name, price, imageUrl }) => (
-  <div className="collection-item">
-    <div
-      className="image"
-      style={{
-        backgroundImage: `url(${imageUrl})`
-      }}>
+const CollectionItem = ({ item, addItem }) => {
+  const { name, price, imageUrl } = item;
+  return (
+    <div className="collection-item">
+      <div
+        className="image"
+        style={{
+          backgroundImage: `url(${imageUrl})`
+        }}>
+      </div>
+
+      <div className="collection-footer">
+        <span className="name">{name}</span>
+        <span className="price">{price}</span>
+      </div>
+      <Button inverted onClick={() => addItem(item)}>Add to cart</Button>
     </div>
+  )
+};
 
-    <div className="collection-footer">
-      <span className="name">{name}</span>
-      <span className="price">{price}</span>
-    </div>
-  </div>
-)
+const mapDispatchToProps = dispatch => ({
+  addItem: item => dispatch(addItem(item))
+})
 
 
-export default CollectionItem;
+export default connect(null, mapDispatchToProps)(CollectionItem);
