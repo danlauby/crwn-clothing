9e54c6a9a11005f861ecbd7cce79d8337b41627d
Daniel Lauby <dmlauby@gmail.com>
2019-11-01 14:54:05 -0700

    Move shop data to redux. Create a shop collections selector and connect to component.

--- src/redux/root-reducer.js
+++ src/redux/root-reducer.js
@@ -2,9 +2,10 @@ import { combineReducers } from 'redux';
 import { persistReducer } from 'redux-persist';
 import storage from 'redux-persist/lib/storage';
 
-import userReducer from './user/user.reducer';
 import cartReducer from './cart/cart.reducer';
 import directoryReducer from './directory/directory.reducer';
+import shopReducer from './shop/shop.reducer';
+import userReducer from './user/user.reducer';
 
 const persistConfig = {
   key: 'root',
@@ -13,9 +14,10 @@ const persistConfig = {
 };
 
 const rootReducer = combineReducers({
-  user: userReducer,
   cart: cartReducer,
-  directory: directoryReducer
+  directory: directoryReducer,
+  shop: shopReducer,
+  user: userReducer
 });
 
 export default persistReducer(persistConfig, rootReducer);
