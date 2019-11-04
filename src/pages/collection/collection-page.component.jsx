2485ff340e4108b9619fab6eeb97742f3e30597c
Daniel Lauby <dmlauby@gmail.com>
2019-11-04 13:45:47 -0800

    Moved directory and shop data into into it's own reducer, created respective selectors and updated cooesponding components with redux flow for directory/shop. Linked directory collections by url object key parameters.

--- src/pages/collection/collection-page.component.jsx
+++ src/pages/collection/collection-page.component.jsx
@@ -8,15 +8,21 @@ import CollectionItem from '../../components/collection-item/collection-item.com
 import './collection-page.styles.scss';
 
 const CollectionPage = ({ collection }) => {
-  console.log(collection);
-return (
-  <div className="collection-page">
-    <h2>Collection Page</h2>
-  </div>
-)};
+  const { title, items } = collection;
+  return (
+    <div className="collection-page">
+      <h2 className="title">{ title }</h2>
+      <div className="items">
+        {
+          items.map(item => <CollectionItem key={item.id} item={item} />)
+        }
+      </div>
+    </div>
+  );
+};
 
-const mapStateToProps = (state, ownProps) => ({
-  collection: selectCollection(ownProps.match.params.collectionId)(state)
-});
+  const mapStateToProps = (state, ownProps) => ({
+    collection: selectCollection(ownProps.match.params.collectionId)(state)
+  });
 
-export default connect(mapStateToProps)(CollectionPage);
+  export default connect(mapStateToProps)(CollectionPage);
